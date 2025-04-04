658b73475095d22e943d6a4b24463d751bb95e8f
Jon Coe <jonmcoe@gmail.com>
2025-04-04 06:05:33 -0400

    2025 catchup

--- fetch.py
+++ fetch.py
@@ -11,17 +11,19 @@ def _save_image(url, papername):
     return path
 
 
-def download_nypost():
-    today = datetime.date.today()
+def download_nypost(d=None):
+    today = d or datetime.date.today()
     today_string = today.strftime('%B-%d-%Y').lower().replace('-0', '-')
     page_html = requests.get(f'https://nypost.com/cover/{today_string}/').content
     soup = bs4.BeautifulSoup(page_html, features="html.parser")
-    image_url = soup.find('source').attrs['data-srcset'].split('?')[0]
+    image_element = soup.find('img', attrs={'class': 'cover-swap__image cover-swap__image--front'})
+    image_url = image_element.attrs['srcset'].split('?')[0]
     return _save_image(image_url, 'nypost')
 
 
-def download_dailynews():
-    day = datetime.date.today().day
+def download_dailynews(d):
+    d = d or datetime.date.today()
+    day = d.day
     # https://www.freedomforum.org/todaysfrontpages/?tfp_display=gallery&tfp_region=USA&tfp_sort_by=state&tfp_state_letter=N
     return _save_image(f'https://cdn.freedomforum.org/dfp/jpg{day}/lg/NY_DN.jpg', 'dailynews')
 
--- post_today.py
+++ post_today.py
@@ -1,12 +1,17 @@
 import datetime
+import sys
 
 import combine
 import fetch
 import tweet
 
 if __name__ == '__main__':
-    daily = fetch.download_dailynews()
-    post = fetch.download_nypost()
+    if len(sys.argv) > 1:
+        dt = datetime.date.fromisoformat(sys.argv[1])
+    else:
+        dt = None
+    daily = fetch.download_dailynews(dt)
+    post = fetch.download_nypost(dt)
     combined = combine.combine(daily, post, f'./generated_images/{datetime.date.today().isoformat()}-combined.jpg')
     status = tweet.tweet_single_image(combined)
     print(status.text)
