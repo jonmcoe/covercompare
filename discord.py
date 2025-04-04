b26627b07c6745ed9f5947566114eff5c6770e13
Jon Coe <jonmcoe@gmail.com>
2025-04-04 06:12:21 -0400

    change from twitter to discord

---
+++ discord.py
@@ -0,0 +1,24 @@
+import datetime
+import env
+import requests
+
+def post(path):
+
+    webhook_url = env.get('COVERCOMPARE_DISCORD_WEBHOOK')
+
+    current_date = datetime.now()
+    formatted_date = current_date.strftime("%A, %B %d")
+
+    # Open the image file in binary mode
+    with open(path, "rb") as f:
+        files = {
+            "file": ("image.jpg", f)
+        }
+        payload = {
+            "payload_json": (
+                '{"content": ' + formatted_date + ', '
+                '"embeds": [{"image": {"url": "attachment://image.jpg"}}]}'
+            )
+        }
+        response = requests.post(webhook_url, data=payload, files=files)
+        return response
